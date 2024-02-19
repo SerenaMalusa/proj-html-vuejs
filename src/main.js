39b320bf8f8f5c7e5467d8f382de93b98864206e
SerenaMalusa <sere.malu@gmail.com>
2024-02-19 15:50:15 +0100

    feat: import bootstrap and fontAwsome

--- src/main.js
+++ src/main.js
@@ -2,9 +2,22 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// Import Bootstrap's CSS
+import "bootstrap/scss/bootstrap";
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
+/* import specific icons  from the 3 free kits*/
+import {  } from '@fortawesome/free-solid-svg-icons';
+import {  } from '@fortawesome/free-regular-svg-icons';
+import {  } from '@fortawesome/free-brands-svg-icons';
+/* add icons to the library */
+library.add(  );
+
 
 // use createApp to create the const app then mount it
 const app = createApp(App);
 // global components
+app.component('font-awesome-icon', FontAwesomeIcon);
 
 app.mount('#app');
