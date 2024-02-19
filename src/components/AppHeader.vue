078a645c9f71dbd9a05dd3db65afbf19ace765c4
SerenaMalusa <sere.malu@gmail.com>
2024-02-19 16:58:38 +0100

    feat: add header base structure

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,8 +5,23 @@
 </script>
 
 <template>
-  <div>
-    Header
+  <div class="wrapper d-flex justify-content-between align-items-end mt-4 debug">
+    
+    <a class="debug h4 text-serif mb-0" href="#">Everlead<b class="text-secondary">.</b></a>
+
+    <nav class="debug">
+      <ul class="d-flex mb-0">
+        <li class="text-uppercase ms-4">Lorem</li>
+        <li class="text-uppercase ms-4">Lorem</li>
+        <li class="text-uppercase ms-4">Lorem</li>
+        <li class="text-uppercase ms-4">Lorem</li>
+        <li class="text-uppercase ms-4">Lorem</li>
+        <li class="text-uppercase ms-4">Lorem</li>
+        <li class="text-uppercase ms-4">Lorem</li>
+        <li class="text-uppercase ms-4">Lorem</li>
+      </ul>
+    </nav>
+
   </div>
 </template>
 
