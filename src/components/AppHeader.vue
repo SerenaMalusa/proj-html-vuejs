bf95120cf2fd3125fd0be6507728c381ce6c15ff
SerenaMalusa <sere.malu@gmail.com>
2024-02-20 16:23:51 +0100

    feat: create component headerNavBar and import data trough props

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,8 +1,11 @@
 <script>
+
+  import HeaderNavBar from './HeaderNavBar.vue';
+
   export default {
     data () {
       return {
-        menu: [
+        menuLinks: [
           {
             link: '#',
             text: 'Home',
@@ -68,17 +71,11 @@
             },
           },
         ],
-        activeIndex: 0,
+        
       }
     },
-    methods: {
-      activateLink(index) {
-        this.activeIndex = index;
-        setTimeout(() => {
-          this.activeIndex = 0;
-        }, 1000 * 20);
-      },
-  
+    components: {
+      HeaderNavBar,
     }
   }
 </script>
@@ -89,26 +86,7 @@
     <img src="../assets/img/logo-img-01.png" alt="Everleadlogo">
     
     <!-- <a class="h4 text-serif mb-0" href="#">Everlead<b class="text-secondary">.</b></a> -->
-
-    <nav>
-      <ul class="d-flex mb-0">
-        <!-- <li class="text-uppercase ms-4">Lorem</li> -->
-        <li 
-        v-for="(link, i) in menu"
-        @mouseenter="activateLink(i)"
-        class="text-uppercase ms-4 d-flex align-items-end"
-        > 
-          <img 
-          :class="(activeIndex == i && activeIndex != menu.length - 1 && activeIndex != menu.length - 2) ? '' : 'd-none'" 
-          class="arrow arrow-right" src="../assets/svg/svg-6.svg" alt="right arrow">
-          <a v-if="link.text" :class="(activeIndex == i) ? 'active' : ''" href="link.link">{{ link.text }}</a>
-          <a v-else href="link.link">
-            <font-awesome-icon :icon="`fa-${link.icon.kitClass} fa-${link.icon.iconClass}`" />
-          </a>
-        </li>
-        
-      </ul>
-    </nav>
+    <header-nav-bar :menu="menuLinks"/>
 
   </div>
 </template>
@@ -123,27 +101,6 @@
       height: $header-h;
       position: relative;
       z-index: 1;
-
-      .active {
-        color: $secondary-text;
-      }
-
-      .arrow {
-        margin: 5px 5px 0 0;
-        height: 20px;
-        width: auto;
-        filter: brightness(0) saturate(100%) invert(44%) sepia(95%) saturate(4667%) hue-rotate(354deg) brightness(101%) contrast(99%);
-      }
-        
-      img {
-        width: 120px;
-        margin-bottom: 5px;
-      }
-
-      a {
-        font-size: 0.9rem;
-        font-weight: 600;
-      }
     }
 
 
---
+++ src/components/HeaderNavBar.vue
@@ -0,0 +1,73 @@
+<script>
+    export default {
+        props: {
+            menu: Array,
+        },
+        data() {
+            return {
+                activeIndex: 0,
+            }
+        },
+        methods: {
+            activateLink(index) {
+                this.activeIndex = index;
+                setTimeout(() => {
+                this.activeIndex = 0;
+                }, 1000 * 20);
+            },
+        },
+    }
+</script>
+
+<template>
+
+        <nav>
+            <ul class="d-flex mb-0">
+                <!-- <li class="text-uppercase ms-4">Lorem</li> -->
+                <li 
+                v-for="(link, i) in menu"
+                @mouseenter="activateLink(i)"
+                class="text-uppercase ms-4 d-flex align-items-end"
+                > 
+                <img 
+                :class="(activeIndex == i && activeIndex != menu.length - 1 && activeIndex != menu.length - 2) ? '' : 'd-none'" 
+                class="arrow arrow-right" src="../assets/svg/svg-6.svg" alt="right arrow">
+                <a v-if="link.text" :class="(activeIndex == i) ? 'active' : ''" href="link.link">{{ link.text }}</a>
+                <a v-else href="link.link">
+                    <font-awesome-icon :icon="`fa-${link.icon.kitClass} fa-${link.icon.iconClass}`" />
+                </a>
+                </li>
+                
+            </ul>
+        </nav>
+
+</template>
+
+<style lang="scss" scoped>
+
+    // import varibales and mixins
+    @use '../styles/partials/variables.scss' as *;
+    @use '../styles/partials/mixins.scss' as *;
+
+    .active {
+        color: $secondary-text;
+    }
+
+    .arrow {
+        margin: 5px 5px 0 0;
+        height: 20px;
+        width: auto;
+        filter: brightness(0) saturate(100%) invert(44%) sepia(95%) saturate(4667%) hue-rotate(354deg) brightness(101%) contrast(99%);
+    }
+        
+    img {
+        width: 120px;
+        margin-bottom: 5px;
+    }
+
+    a {
+        font-size: 0.9rem;
+        font-weight: 600;
+    }
+
+</style>
