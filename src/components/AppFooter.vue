e261e7d3cffce68c6daed43aa265f7e065b71534
SerenaMalusa <sere.malu@gmail.com>
2024-02-20 13:05:01 +0100

    feat: add footer copiright section

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -6,10 +6,35 @@
 
 <template>
     <div>
-        Footer
+        
+        <div class="container-l">
+            <div class="container-s">
+
+            </div>
+        </div>
+
+        <div class="copyright">
+            Copyrigth Everlead©MikadoTeams
+        </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
+
+    // import varibales and mixins
+    @use '../styles/partials/variables.scss' as *;
+    @use '../styles/partials/mixins.scss' as *;
+
+    div {
+        background-color: #181515;
+        color: $footer-text;
+
+        .copyright {
+            padding: calc($base-space / 3);
+            background-color: $secondary-bg;
+            text-align: center;
+            font-size: 0.5rem;
+        }
+    }
 
 </style>
