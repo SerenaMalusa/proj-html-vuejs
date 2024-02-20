0723366073103543dbb0bee63969d2d50c6b5e5c
SerenaMalusa <sere.malu@gmail.com>
2024-02-20 13:18:57 +0100

    feat: add footer base structure

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -8,14 +8,42 @@
     <div>
         
         <div class="container-l">
-            <div class="container-s">
+            <div class="container-s d-flex gap-2">
+                <div>
 
+                    <h6 class="text-serif">About</h6>
+                    <div class="divider"></div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos doloremque animi exercitationem iusto pariatur distinctio accusamus culpa dicta neque! Qui!</p>
+
+                </div>
+
+                <div>
+                    
+                    <h6 class="text-serif">About</h6>
+                    <div class="divider"></div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos doloremque animi exercitationem iusto pariatur distinctio accusamus culpa dicta neque! Qui!</p>
+
+                </div>
+                <div>
+                    
+                    <h6 class="text-serif">About</h6>
+                    <div class="divider"></div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos doloremque animi exercitationem iusto pariatur distinctio accusamus culpa dicta neque! Qui!</p>
+
+                </div>
+                <div>
+                    
+                    <h6 class="text-serif">About</h6>
+                    <div class="divider"></div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos doloremque animi exercitationem iusto pariatur distinctio accusamus culpa dicta neque! Qui!</p>
+
+                </div>
             </div>
         </div>
 
-        <div class="copyright">
+        <aside class="copyright">
             Copyrigth Everlead©MikadoTeams
-        </div>
+        </aside>
     </div>
 </template>
 
@@ -28,6 +56,20 @@
     div {
         background-color: #181515;
         color: $footer-text;
+
+        .debug {
+            border: 1px solid white;
+        }
+
+        .container-s {
+            padding: 60px 0;
+
+            .divider {
+                background-color: $secondary-bg;
+                width: 30px;
+                height: 1px;
+            }
+        }
 
         .copyright {
             padding: calc($base-space / 3);
