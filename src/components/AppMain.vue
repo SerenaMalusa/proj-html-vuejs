28cb99347c99b2ed0e67c90e5de30220197c4e2e
SerenaMalusa <sere.malu@gmail.com>
2024-02-20 11:14:12 +0100

    feat: add structure for section events

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -103,6 +103,61 @@
                 </div>
             </div>
         </section>
+        <!-- END SECTION FOUNDER -->
+
+        <!-- SECTION EVENTS -->
+        <section id="events">
+            <div class="container-l">
+                <div class="container-s">
+                    <ul class="col-5">
+                        <div class="section-title py-3 px-5">
+                            <h4 class="text-capitalize text-serif">Upcoming events</h4> 
+                        </div>
+
+                        <li class="active px-5 py-3 d-flex gap-3 align-items-baseline">
+                            <span class="event-date">
+                                <div class="event-day text-serif">07</div>
+                                <div class="event-month">Jan. 2024</div>
+                            </span>
+
+                            <span class="event-info">
+                                <h6 class="h6 text-serif">Lorem</h6>
+                                <p class="text-info mb-1">Lorem ipsum dolor sit amet.</p>
+                                <p class="text-info mb-1">Lorem ipsum dolor sit amet.</p>
+                                <a href="#">READ MORE</a>
+                            </span>
+                        </li>
+
+                        <li class="px-5 py-3 d-flex gap-3 align-items-baseline">
+                            <span class="event-date">
+                                <div class="event-day text-serif">07</div>
+                                <div class="event-month">Jan. 2024</div>
+                            </span>
+
+                            <span class="event-info">
+                                <h6 class="h6 text-serif">Lorem</h6>
+                                <p class="text-info mb-1">Lorem ipsum dolor sit amet.</p>
+                                <p class="text-info mb-1">Lorem ipsum dolor sit amet.</p>
+                                <a href="#">READ MORE</a>
+                            </span>
+                        </li>
+                        <li class="px-5 py-3 d-flex gap-3 align-items-baseline">
+                            <span class="event-date">
+                                <div class="event-day text-serif">07</div>
+                                <div class="event-month">Jan. 2024</div>
+                            </span>
+
+                            <span class="event-info">
+                                <h6 class="h6 text-serif">Lorem</h6>
+                                <p class="text-info mb-1">Lorem ipsum dolor sit amet.</p>
+                                <p class="text-info mb-1">Lorem ipsum dolor sit amet.</p>
+                                <a href="#">READ MORE</a>
+                            </span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
     </div>
 </template>
 
@@ -166,10 +221,13 @@
 
                 position: absolute;
                 top: 50%;
-                transform: translate(0, 50%);
 
                 &.arrow-right {
                     left: 100%;
+                }
+
+                &.arrow-left {
+                    transform: rotate(180deg);
                 }
 
             }
@@ -233,6 +291,55 @@
             position: relative;
             left: calc($base-space * -5);
         }
+    }
+
+    #events {
+        background-image: url(../assets/img/h1-img-09.jpg);
+        background-size: cover;
+        background-repeat: no-repeat;
+
+        .container-s {
+            padding: calc($base-space * 5) 0;
+
+            ul {
+                background-color: $main-bg;
+
+                .section-title {
+                        margin: 0;
+                        box-shadow: 1px 11px 5px -3px rgba(191,191,191,0.75);
+
+                        position: relative;
+                        z-index: 2;
+                    }
+
+                li {
+                    border-top: 1px solid #f1f1f1;
+
+                    &.active {
+                        background-color: $highlight-bg;
+                    }                    
+    
+                    .event-date {
+                        padding: calc($base-space / 4) calc($base-space / 2);
+                        background-color: $secondary-bg;
+                        color: $highlight-bg;
+                        text-align: center;
+                        font-size: 0.8rem;
+    
+                        .event-month {
+                            font-size: 0.6rem;
+                        }
+                    }
+    
+                    .event-info {
+                        font-size: 0.8rem;
+                    }
+
+                }
+            }
+
+        }
+
     }
 
 </style>
