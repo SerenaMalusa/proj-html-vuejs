ef9e6dfafd78e04504a1e9787a6f74c0cd103109
SerenaMalusa <sere.malu@gmail.com>
2024-02-20 15:49:59 +0100

    feat: dinamically add header nav elements

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,24 +1,95 @@
 <script>
   export default {
-    
+    data () {
+      return {
+        menu: [
+          {
+            link: '#',
+            text: 'Home',
+            icon: {
+              kitClass: '',
+              iconClass: '',
+            },
+          },
+          {
+            link: '#',
+            text: 'Pages',
+            icon: {
+              kitClass: '',
+              iconClass: '',
+            },
+          },
+          {
+            link: '#',
+            text: 'Blog',
+            icon: {
+              kitClass: '',
+              iconClass: '',
+            },
+          },
+          {
+            link: '#',
+            text: 'Shop',
+            icon: {
+              kitClass: '',
+              iconClass: '',
+            },
+          },
+          {
+            link: '#',
+            text: 'Events',
+            icon: {
+              kitClass: '',
+              iconClass: '',
+            },
+          },
+          {
+            link: '#',
+            text: 'Elements',
+            icon: {
+              kitClass: '',
+              iconClass: '',
+            },
+          },
+          {
+            link: '#',
+            text: '',
+            icon: {
+              kitClass: 'solid',
+              iconClass: 'magnifying-glass',
+            },
+          },
+          {
+            link: '#',
+            text: '',
+            icon: {
+              kitClass: 'solid',
+              iconClass: 'bars',
+            },
+          },
+        ]
+      }
+    }
   }
 </script>
 
 <template>
-  <div class="container-l d-flex justify-content-between align-items-end debug">
+  <div class="container-l d-flex justify-content-between align-items-end">
+
+    <img src="../assets/img/logo-img-01.png" alt="Everleadlogo">
     
-    <a class="h4 text-serif mb-0" href="#">Everlead<b class="text-secondary">.</b></a>
+    <!-- <a class="h4 text-serif mb-0" href="#">Everlead<b class="text-secondary">.</b></a> -->
 
     <nav>
       <ul class="d-flex mb-0">
-        <li class="text-uppercase ms-4">Lorem</li>
-        <li class="text-uppercase ms-4">Lorem</li>
-        <li class="text-uppercase ms-4">Lorem</li>
-        <li class="text-uppercase ms-4">Lorem</li>
-        <li class="text-uppercase ms-4">Lorem</li>
-        <li class="text-uppercase ms-4">Lorem</li>
-        <li class="text-uppercase ms-4">Lorem</li>
-        <li class="text-uppercase ms-4">Lorem</li>
+        <!-- <li class="text-uppercase ms-4">Lorem</li> -->
+        <li v-for="link in menu" class="text-uppercase ms-4"> 
+          <a v-if="link.text" href="link.link">{{ link.text }}</a>
+          <a v-else href="link.link">
+            <font-awesome-icon :icon="`fa-${link.icon.kitClass} fa-${link.icon.iconClass}`" />
+          </a>
+        </li>
+        
       </ul>
     </nav>
 
@@ -32,9 +103,19 @@
     @use '../styles/partials/mixins.scss' as *;
 
     .container-l {
-      // height: $header-h;
+      height: $header-h;
       position: relative;
       z-index: 1;
+        
+      img {
+        width: 120px;
+        margin-bottom: 5px;
+      }
+
+      a {
+        font-size: 0.9rem;
+        font-weight: 600;
+      }
     }
 
 
--- src/main.js
+++ src/main.js
@@ -10,11 +10,11 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 /* import specific icons  from the 3 free kits*/
-import { faLocationDot, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faLocationDot, faPhone, faEnvelope, faMagnifyingGlass, faBars } from '@fortawesome/free-solid-svg-icons';
 import {  } from '@fortawesome/free-regular-svg-icons';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 /* add icons to the library */
-library.add( faTwitter, faLocationDot, faPhone, faEnvelope );
+library.add( faTwitter, faLocationDot, faPhone, faEnvelope, faMagnifyingGlass, faBars );
 
 // import globall components
 import AppSocial from './components/AppSocial.vue';
