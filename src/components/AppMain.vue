d2075650b14b390b83437088f2c617da012d489a
SerenaMalusa <sere.malu@gmail.com>
2024-02-21 10:28:42 +0100

    feat: create and import Main Events

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -4,12 +4,14 @@
     import MainHero from './MainHero.vue';
     import MainSpecialties from './MainSpecialties.vue';
     import MainFounders from './MainFounders.vue';
+    import MainEvents from './MainEvents.vue';
 
     export default {
         components: {
             MainHero,
             MainSpecialties,
             MainFounders,
+            MainEvents,
         }
     }
 </script>
@@ -30,59 +32,7 @@
         <main-founders />
 
         <!-- SECTION EVENTS -->
-        <section class="section-padding" id="events">
-            <div class="container-l">
-                <div class="container-s">
-                    <ul class="col-5">
-                        <div class="section-title py-3 px-5">
-                            <h4 class="text-capitalize text-serif">Upcoming events</h4> 
-                        </div>
-
-                        <li class="active px-5 py-3 d-flex gap-3 align-items-baseline">
-                            <span class="event-date">
-                                <div class="event-day text-serif">07</div>
-                                <div class="event-month">Jan. 2024</div>
-                            </span>
-
-                            <span class="event-info">
-                                <h6 class="h6 text-serif">Lorem</h6>
-                                <p class="text-info mb-1">Lorem ipsum dolor sit amet.</p>
-                                <p class="text-info mb-1">Lorem ipsum dolor sit amet.</p>
-                                <a href="#">READ MORE</a>
-                            </span>
-                        </li>
-
-                        <li class="px-5 py-3 d-flex gap-3 align-items-baseline">
-                            <span class="event-date">
-                                <div class="event-day text-serif">07</div>
-                                <div class="event-month">Jan. 2024</div>
-                            </span>
-
-                            <span class="event-info">
-                                <h6 class="h6 text-serif">Lorem</h6>
-                                <p class="text-info mb-1">Lorem ipsum dolor sit amet.</p>
-                                <p class="text-info mb-1">Lorem ipsum dolor sit amet.</p>
-                                <a href="#">READ MORE</a>
-                            </span>
-                        </li>
-                        <li class="px-5 py-3 d-flex gap-3 align-items-baseline">
-                            <span class="event-date">
-                                <div class="event-day text-serif">07</div>
-                                <div class="event-month">Jan. 2024</div>
-                            </span>
-
-                            <span class="event-info">
-                                <h6 class="h6 text-serif">Lorem</h6>
-                                <p class="text-info mb-1">Lorem ipsum dolor sit amet.</p>
-                                <p class="text-info mb-1">Lorem ipsum dolor sit amet.</p>
-                                <a href="#">READ MORE</a>
-                            </span>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </section>
-        <!-- END SECTION EVENTS -->
+        <main-events />
 
         <!-- SECTION LEADER -->
         <section class="section-padding" id="leader">
@@ -164,53 +114,6 @@
 
         .section-padding {
             padding: calc($base-space * 5) 0;
-        }
-
-    }
-
-    #events {
-        background-image: url(../assets/img/h1-img-09.jpg);
-        background-size: cover;
-        background-repeat: no-repeat;
-
-        .container-s {
-            ul {
-                background-color: $main-bg;
-
-                .section-title {
-                        margin: 0;
-                        box-shadow: 1px 11px 5px -3px rgba(191,191,191,0.75);
-
-                        position: relative;
-                        z-index: 2;
-                    }
-
-                li {
-                    border-top: 1px solid #f1f1f1;
-
-                    &.active {
-                        background-color: $highlight-bg;
-                    }                    
-    
-                    .event-date {
-                        padding: calc($base-space / 4) calc($base-space / 2);
-                        background-color: $secondary-bg;
-                        color: $highlight-bg;
-                        text-align: center;
-                        font-size: 0.8rem;
-    
-                        .event-month {
-                            font-size: 0.6rem;
-                        }
-                    }
-    
-                    .event-info {
-                        font-size: 0.8rem;
-                    }
-
-                }
-            }
-
         }
 
     }
